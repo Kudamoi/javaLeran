b5f249d3e09a638ac982bd2aa98a06f8f839e7e2
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-23 22:39:10 +0300

    Lesson 4.2 complete

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE + " < max value byte and min value byte > " + Byte.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE + " < max value int and min value int > " + Integer.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE + " < max value short and min value short > " + Short.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE + " < max value long and min value long > " + Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE + " < max value float and min value float > " + Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE + " < max value double and min value double > " + Double.MIN_VALUE);
+    }
+}
