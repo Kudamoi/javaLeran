fce9971ff60ce719b429c7a6ae56c769d95f9f7e
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-24 02:46:50 +0300

    Lesson 4.4.1 complete

---
+++ 04_NumbersStringsAndDates/homework_4.4/TypeAlphabet/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.print(Character.toChars(i));
+            System.out.print(" ");
+        }
+        System.out.println();
+        for (int i = 97; i <= 122; i++) {
+            System.out.print(Character.toChars(i));
+            System.out.print(" ");
+        }
+
+    }
+}
