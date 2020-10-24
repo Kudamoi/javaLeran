9c772151cf5dbdfc3c7305d1801d475cd87db064
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-24 13:43:43 +0300

    Lesson 4.4.1 rewrite

--- 04_NumbersStringsAndDates/homework_4.4/TypeAlphabet/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TypeAlphabet/src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 65; i <= 90; i++) {
-            System.out.print("Символ - ");
-            System.out.print(Character.toChars(i));
-            System.out.println(" Код - " + i);
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.println("Cимвол - " + i + " Код - " + (int) i);
         }
-        System.out.println();
-        for (int i = 97; i <= 122; i++) {
-            System.out.print("Символ - ");
-            System.out.print(Character.toChars(i));
-            System.out.println(" Код - " + i);
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println("Cимвол - " + i + " Код - " + (int) i);
         }
-
     }
 }
