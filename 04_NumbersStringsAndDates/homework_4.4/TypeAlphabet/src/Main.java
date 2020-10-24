9d159211a84baec6a132b509d897d2146beeb227
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-24 03:11:07 +0300

    Lesson 4.4.1 recommit complete

--- 04_NumbersStringsAndDates/homework_4.4/TypeAlphabet/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TypeAlphabet/src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 65; i <= 90; i++) {
+            System.out.print("Символ - ");
             System.out.print(Character.toChars(i));
-            System.out.print(" ");
+            System.out.println(" Код - " + i);
         }
         System.out.println();
         for (int i = 97; i <= 122; i++) {
+            System.out.print("Символ - ");
             System.out.print(Character.toChars(i));
-            System.out.print(" ");
+            System.out.println(" Код - " + i);
         }
 
     }
