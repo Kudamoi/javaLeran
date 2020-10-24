49d74e73deee1a91549f3b53a1e51927be1b34ae
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-24 20:56:00 +0300

    Lesson 4.4.3 complete

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -7,9 +7,38 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
+      for (int i = 0; i < input.length(); i++) {
+        if (input.charAt(i) == ' ' || input.charAt(i) == '-')
+          continue;
+        if (!Character.isLetter(input.charAt(i))) {
+          System.out.println("Введенная строка не является ФИО");
+          return;
+        }
       }
+      String firstName = "", secondName = "", thirdName = "";
+      try {
+        firstName = input.substring(0, input.indexOf(" "));
+        String firstPath = input.substring(firstName.length()).trim();
+        secondName = firstPath.substring(0, firstPath.indexOf(" ")).trim();
+        if (firstPath.indexOf(" ") == firstPath.lastIndexOf(" "))
+          thirdName = firstPath.substring(firstPath.lastIndexOf(" ")).trim();
+        else {
+          System.out.println("Введенная строка не является ФИО");
+          return;
+        }
+        System.out.println("Фамилия: " + firstName);
+        System.out.println("Имя: " + secondName);
+        System.out.println("Отчество: " + thirdName);
+      }
+      catch (Exception e) {
+        System.out.println("Введенная строка не является ФИО");
+        return;
+      }
+
+      return;
+      /*if (input.equals("0")) {
+        break;
+      }*/
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
