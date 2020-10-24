3de8cae86cdfd58b6b61f535dd69f6367b3a9803
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-24 04:52:07 +0300

    Lesson 4.4.2 complete

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+    String first = text.substring(text.indexOf(" ") + 1);
+    String sub = text.substring(text.indexOf("-") + 1, text.lastIndexOf("руб")).trim();
+
+    System.out.println(Integer.parseInt(first.substring(first.indexOf(" "), first.indexOf("руб")).trim()) + (int) Integer.parseInt(sub.substring(0, sub.indexOf("руб")).trim()) + Integer.parseInt(sub.substring(sub.lastIndexOf(" ")).trim()));
   }
 
 }
