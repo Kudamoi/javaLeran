6394a25818352f7b127f5d0a1f15fe624ff29a21
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 23:18:26 +0300

    Lesson 4.5.2 complete

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static String splitTextInToWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+    public static String splitTextInToWords(String text) {
+        return text.replaceAll("[0-9,.;-]", " ").trim().replaceAll("\\s+", " ").replaceAll("[\\s]", "\r\n");
+    }
 
 }
