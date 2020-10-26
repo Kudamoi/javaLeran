8cbfb339cb7badcea2efed20b1af6bc685591375
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 23:26:42 +0300

    Lesson 4.5.5 complete

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+        return text.replaceAll("<....>", placeholder).replaceAll("<.+>", placeholder);
     }
 
 }
