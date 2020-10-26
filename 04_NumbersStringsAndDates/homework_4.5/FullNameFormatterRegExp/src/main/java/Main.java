bcf774653ae9d3d20027950cd4d466f062ae656f
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 22:13:43 +0300

    Lesson 4.5.3 complete

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            String[] FIO = input.split(" ");
+
+            if (FIO.length != 3 || input.matches(".+\\d.+"))
+                System.out.println("Введенная строка не является ФИО");
+            else {
+                System.out.println("Фамилия: " + FIO[0]);
+                System.out.println("Имя: " + FIO[1]);
+                System.out.println("Отчество: " + FIO[2]);
+            }
+
+            break;
+        }
     }
-  }
 
 }
