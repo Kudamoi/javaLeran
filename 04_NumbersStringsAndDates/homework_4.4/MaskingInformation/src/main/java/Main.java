3050c8f75bec1a4c81d650f3dae77535f25ee212
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 13:29:41 +0300

    Lesson 4.4.4 complete

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -6,7 +6,25 @@ public class Main {
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        while (checkRectangleBracket(text)){
+            text = text.replace(text.substring(text.indexOf("<"), text.indexOf(">") + 1), placeholder);
+        }
+        System.out.println(text);
         return text;
+    }
+
+    public static boolean checkRectangleBracket(String text) {
+        boolean leftRectangularBracket = false;
+        boolean rightRectangularBracket = false;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '<')
+                leftRectangularBracket = true;
+            if (text.charAt(i) == '>')
+                rightRectangularBracket = true;
+        }
+        if (leftRectangularBracket != rightRectangularBracket || leftRectangularBracket == false && rightRectangularBracket == false)
+            return false;
+        return true;
     }
 
 }
