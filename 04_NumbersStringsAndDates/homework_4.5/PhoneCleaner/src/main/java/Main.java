be6475782693bafa9a462e77e60836f8ee149971
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 22:59:07 +0300

    Lesson 4.5.4 complete

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -2,16 +2,30 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            String clear = input.replaceAll("[^0-9]", "");
+            StringBuilder clearTelephone = new StringBuilder(clear);
+            switch (clear.length()) {
+                case 10:
+                    clearTelephone.replace(0, 0, "7");
+                    break;
+                case 11:
+                    if (clearTelephone.indexOf("7") == 0 || clearTelephone.indexOf("8") == 0)
+                        clearTelephone.replace(0, 1, "7");
+                    else
+                        clearTelephone.delete(0, clearTelephone.length()).append("Неверный формат номера");
+                    break;
+                default:
+                    clearTelephone.delete(0, clearTelephone.length()).append("Неверный формат номера");
+            }
+            System.out.println(clearTelephone.toString());
+            break;
+        }
     }
-  }
 
 }
