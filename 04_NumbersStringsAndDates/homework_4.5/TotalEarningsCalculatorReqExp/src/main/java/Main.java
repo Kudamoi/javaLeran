16c9b3b3adb270d2383d6c7ec31c9d6dc5a99765
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-10-26 17:51:46 +0300

    Lesson 4.5.1 complete

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    return -1;
+    int result = 0;
+    String[] earnings = text.split("руб");
+    for (int i = 0; i < earnings.length - 1; i++)
+     result += Integer.parseInt(earnings[i].substring(earnings[i].trim().lastIndexOf(" ")).trim());
+
+    return result;
   }
 
 }
