0626a549a50669647995a43706588d34c137bc8f
Гавриленко Александр <lihoy.sasha@gmail.com>
2020-11-03 14:10:09 +0300

    Lesson 8.1

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,33 +1,38 @@
 import java.util.HashMap;
+import java.util.stream.Stream;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+        String name = "";
+        if (components.length != 4)
+            throw new IllegalArgumentException("Не верно введено количество аргументов.");
+        else {
+            System.out.println(components[0] + " " + components[1] + " " + components[2] + " " + components[3]);
+            if (components[0].matches("[а-яА-Я]+") && components[1].matches("[а-яА-Я]+"))
+                name = components[0] + " " + components[1];
+            else throw new IllegalArgumentException("Не верно введены имя и/или отчество");
+            if (components[2].matches(".+[@].+") && components[3].matches("[+]?[78]?[0-9]{11}"))
+                storage.put(name, new Customer(name, components[3], components[2]));
+            else throw new IllegalArgumentException("Не верно введен номер и/или email");
+        }
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
