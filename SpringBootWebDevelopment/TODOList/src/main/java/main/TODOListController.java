a5328e39dde17960d5dd2b4e2ca893c07e28cc31
Александр <lihoy.sasha@gmail.com>
2022-05-25 22:40:57 +0300

    t12.3 complete homework

--- SpringBootWebDevelopment/TODOList/src/main/java/main/TODOListController.java
+++ SpringBootWebDevelopment/TODOList/src/main/java/main/TODOListController.java
@@ -1,35 +1,48 @@
 package main;
 
+import main.model.Case;
+import main.model.CaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TODOListController {
 
+    @Autowired
+    private CaseRepository caseRepository;
+
     @RequestMapping(value = "/list/", method = RequestMethod.GET)
-    public List<Case> list()
-    {
-        return Storage.getAllCases();
+    public List<Case> list() {
+        Iterable<Case> iterable = caseRepository.findAll();
+        ArrayList<Case> list = new ArrayList<>();
+        for (Case cas : iterable) {
+            list.add(cas);
+        }
+        return list;
     }
 
     @RequestMapping(value = "/list/", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     public ResponseEntity<?> add(@RequestParam String name) {
-        return new ResponseEntity<>(Storage.add(new Case(name)), HttpStatus.CREATED);
+        Case response = caseRepository.save(new Case(name));
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/list/", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> add(@RequestBody Case cas) {
-        return new ResponseEntity<>(Storage.add(cas), HttpStatus.CREATED);
+        Case response = caseRepository.save(cas);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/list/{id}", method = RequestMethod.DELETE)
     public int remove(@PathVariable int id) {
-       return Storage.delete(id);
+        caseRepository.deleteById(id);
+        return id;
     }
 }
